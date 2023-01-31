3934e589cf20412e3f8f452788a66d8d57dea8db
Josiah Webb <jj.webb520@gmail.com>
2023-01-31 14:14:21 -0700

    fixes screen position

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -6,6 +6,8 @@ export default function Screen() {
             transform
             wrapperClass='htmlScreen'
             distanceFactor={ 1.17 }
+            position={[ 0, 1.56, -1.4 ]}
+            rotation-x={ -.256 }
         >
             <iframe src="https://0xjosiah.xyz/" />
         </Html>
