35b1952b33779cf3eb4598790e850a1c1dee8d8e
Josiah Webb <jj.webb520@gmail.com>
2023-01-31 13:52:17 -0700

    adds html to comp model

--- src/ComputerModel.jsx
+++ src/ComputerModel.jsx
@@ -1,11 +1,13 @@
-import { Center, Float, useGLTF } from "@react-three/drei"
+import { Center, Float, Html, useGLTF } from "@react-three/drei"
 
 export default function ComputerModel(props) {
     const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
 
     return (
         <Float rotationIntensity={ .4 }>
-            <primitive object={ scene } position-y={ -1.2 } />
+            <primitive object={ scene } position-y={ -1.2 }>
+                <Html>hello</Html>
+            </primitive>
         </Float>
     )
 }
