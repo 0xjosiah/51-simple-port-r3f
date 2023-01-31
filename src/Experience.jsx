70c03aea1595a6553a66fc77585f1cb7fe61ec65
Josiah Webb <jj.webb520@gmail.com>
2023-01-31 13:29:09 -0700

    adds config and snap to prez controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -14,6 +14,8 @@ export default function Experience()
             rotation={[ .13, .1, 0 ]}
             polar={[ -.4, .2 ]} // vertical rotation limits
             azimuth={[ -1, .75 ]} // horizontal rotation limits
+            config={{ mass: 2, tension: 400 }} // config is based on Spring lib
+            snap={{ mass: 4, tension: 100 }} // snaps back to og position
         >
             <ComputerModel />
         </PresentationControls>
