139fc226f95b22f1be5f93af8f4af19717dc448f
Josiah Webb <jj.webb520@gmail.com>
2023-01-31 14:04:57 -0700

    adds class and styling to iframe

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -2,9 +2,11 @@ import { Html } from '@react-three/drei'
 
 export default function Screen() {
     return (
-        <Html>
+        <Html
+            transform
+            wrapperClass='htmlScreen'
+        >
             <iframe src="https://0xjosiah.xyz/" />
-            {/* hello */}
         </Html>
     )
 }
