bdc569be65ffd837a4f643d5967160cb286a8a68
Josiah Webb <jj.webb520@gmail.com>
2023-01-31 14:47:57 -0700

    final tweaks to text

--- src/ComputerModel.jsx
+++ src/ComputerModel.jsx
@@ -1,4 +1,4 @@
-import { Center, Float, Html, useGLTF } from "@react-three/drei"
+import { Center, Float, Html, Text, useGLTF } from "@react-three/drei"
 import Screen from "./Screen"
 
 export default function ComputerModel(props) {
@@ -17,6 +17,18 @@ export default function ComputerModel(props) {
             <primitive object={ scene } position-y={ -1.2 }>
                 <Screen />
             </primitive>
+            <Text
+                font="./shadows-into-light-v15-latin-regular.woff"
+                fontSize={ 1 }
+                color='#f0dfe0'
+                position={[ 2, .75, .75 ]}
+                rotation-y={ -1.25 }
+                maxWidth={ 2 }
+                lineHeight={ 1.2 }
+                castShadow={ false }
+            >
+                Josiah Webb
+            </Text>
         </Float>
     )
 }
