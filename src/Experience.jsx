5bf9582959f4bda7ed6db865f40f24a420e8af61
Josiah Webb <jj.webb520@gmail.com>
2023-01-31 13:06:49 -0700

    removes placeholder cube

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -13,10 +13,5 @@ export default function Experience()
         
         <ComputerModel />
         
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
     </>
 }
